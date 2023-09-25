c2518c1558a0298863abb70d058f114c8732dc88
Seth Mwebi <sethmwebi27@gmail.com>
2023-09-25 16:58:24 +0300

    query post

--- src/app/posts/[id].tsx
+++ src/app/posts/[id].tsx
@@ -1,18 +1,37 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, ActivityIndicator } from "react-native";
 import React from "react";
 import posts from "../../../assets/data/posts.json";
 import PostListItem from "@/components/PostListItem";
 import { useLocalSearchParams } from "expo-router";
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query MyQuery($id: ID!) {
+    post(id: $id) {
+      content
+      id
+      image
+      profile {
+        id
+        name
+        image
+        position
+      }
+    }
+  }
+`;
 
 export default function PostDetailsScreen() {
   const { id } = useLocalSearchParams();
+  const { loading, error, data } = useQuery(query, { variables: { id } });
 
-  const post = posts.find((post) => post.id === id);
+  if (loading) return <ActivityIndicator />;
 
-  if (!post) return <Text>Post not found!</Text>;
+  if (error) return <Text>Something went wrong!</Text>;
+
   return (
     <ScrollView>
-      <PostListItem post={post} />
+      <PostListItem post={data.post} />
     </ScrollView>
   );
 }
