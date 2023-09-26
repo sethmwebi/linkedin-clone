94e9c7859fdd291e08b6afd1383501666846ba32
Seth Mwebi <sethmwebi27@gmail.com>
2023-09-26 17:55:06 +0300

    pagination

--- src/apollo/client.tsx
+++ src/apollo/client.tsx
@@ -1,12 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, TypePolicies } from "@apollo/client";
+
+const typePolicies: TypePolicies = {
+  Query: {
+    fields: {
+      postPaginatedList: {
+        keyArgs: false,
+        merge(existing = [], incoming) {
+          return [...existing, ...incoming];
+        }
+      }
+    }
+  }
+};
 
 const client = new ApolloClient({
-  uri: "http://192.168.42.61:9000/api/linkedin/__graphql",
+  uri: "http://192.168.42.140:9000/api/linkedin/__graphql",
   headers: {
     Authorization:
       "apikey graphql::local.net+1000::230cd5297650f3fba42d686efe58676d383242944de4ecbf01d5177167982440"
   },
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({ typePolicies })
 });
 
 export default client;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { ActivityIndicator, FlatList, Text } from "react-native";
 
 import PostListItem from "@/components/PostListItem";
 import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
 // import posts from "../../../assets/data/posts.json";
 
 const postList = gql`
@@ -20,8 +21,40 @@ const postList = gql`
   }
 `;
 
+const postPaginatedList = gql`
+  query PostPaginatedListQuery($first: Int, $after: Int) {
+    postPaginatedList(first: $first, after: $after) {
+      id
+      image
+      content
+      profile {
+        id
+        name
+        position
+        image
+      }
+    }
+  }
+`;
+
 export default function HomeFeedScreen() {
-  const { loading, error, data } = useQuery(postList);
+  const [hasMore, setHasMore] = useState(true);
+  const { loading, error, data, fetchMore } = useQuery(postPaginatedList, {
+    variables: { first: 2 }
+  });
+
+  const loadMore = async () => {
+    if (!hasMore) {
+      return;
+    }
+
+    const res = await fetchMore({
+      variables: { after: data.postPaginatedList.length }
+    });
+    if (res.data.postPaginatedList.length === 0) {
+      setHasMore(false);
+    }
+  };
 
   if (loading) return <ActivityIndicator />;
 
@@ -29,9 +62,10 @@ export default function HomeFeedScreen() {
 
   return (
     <FlatList
-      data={data.postList}
+      data={data.postPaginatedList}
       renderItem={({ item }) => <PostListItem post={item} />}
       showsVerticalScrollIndicator={false}
+      onEndReached={loadMore}
       contentContainerStyle={{ gap: 10 }}
     />
   );
