6bd206780cc2f0287a6d98cef907db99e3941e0e
Seth Mwebi <sethmwebi27@gmail.com>
2023-09-30 17:37:45 +0300

    search functionality

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,29 +1,61 @@
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator
+} from "react-native";
 import users from "../../assets/data/users.json";
 import UserListItem from "@/components/UserListItem";
 import { useLayoutEffect, useState } from "react";
 import { useNavigation } from "expo-router";
+import { gql, useQuery } from "@apollo/client";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const query = gql`
+  query profileSearch($term: String) {
+    profileSearch(term: $term) {
+      id
+      image
+      name
+      position
+    }
+  }
+`;
 
 export default function SearchScreen() {
   const [search, setSearch] = useState("");
-
+  const { data, loading, error } = useQuery(query, {
+    variables: { term: `%${search}%` }
+  });
+  console.log(data?.profileSearch);
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerSearchBarOptions: {
         placeholder: "Search users",
-        onChangeText: setSearch,
-      },
+        onChangeText: (event) => setSearch(event.nativeEvent.text)
+      }
     });
   }, [navigation]);
+
+  if (loading && !data?.profileSearch) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>something went wrong!</Text>;
+  }
+
   return (
-    <View style={{ backgroundColor: "white", flex: 1 }}>
+    <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
       <FlatList
-        data={users}
+        contentContainerStyle={{ marginTop: 150 }}
+        data={data?.profileSearch || []}
         keyExtractor={(item, index) => item.name}
         renderItem={({ item }) => <UserListItem user={item} />}
       />
-    </View>
+    </SafeAreaView>
   );
 }
